9bb50a1da1e9bb3d5d5941ab3997c11b6b3a1b77
Jhony Freitas <jhony00._@hotmail.com>
2023-03-09 20:56:19 -0300

    Adicionando paleta de cores

--- src/constants/variables.js
+++ src/constants/variables.js
@@ -2,5 +2,11 @@ import '../_assets/styles/index.css'
 
 export const variables = {
     primaryFont: 'Bogart',
-    secondaryFont: 'Montserrat, sans-serif'
+    secondaryFont: 'Montserrat, sans-serif',
+    color01: '#0D2840',
+    color02: '#202359',
+    color03: '#322073',
+    color04: '#42208C',
+    color05: '#2632d9',
+    color06: '#50D4F2'
 }
