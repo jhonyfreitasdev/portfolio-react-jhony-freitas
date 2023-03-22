5e2d22ceb56e98a8ebc231cddcf22c04fd7c3ea5
Jhony Freitas <jhony00._@hotmail.com>
2023-03-22 20:44:15 -0300

    ajustando tamanho dos dispositivos

--- src/constants/device.js
+++ src/constants/device.js
@@ -4,8 +4,7 @@ const size = {
     mobileL: '425px',
     tablet: '768px',
     laptop: '1024px',
-    laptopL: '1440px',
-    desktop: '2560px'
+    laptopL: '1440px'
 }
 
 export const device = {
@@ -14,8 +13,6 @@ export const device = {
     mobileL: `(max-width: ${size.mobileL})`,
     tablet: `(max-width: ${size.tablet})`,
     laptop: `(max-width: ${size.laptop})`,
-    laptopL: `(max-width: ${size.laptopL})`,
-    desktop: `(max-width: ${size.desktop})`,
-    desktopL: `(max-width: ${size.desktop})`
+    laptopL: `(max-width: ${size.laptopL})`
 };
 
