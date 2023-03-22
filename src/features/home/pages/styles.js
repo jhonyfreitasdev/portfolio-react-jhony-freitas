b4722ef1ff45814a73f50fce7361f569ff931c1e
Jhony Freitas <jhony00._@hotmail.com>
2023-03-21 22:49:51 -0300

    Arrumando padding da seção home

--- src/features/home/pages/styles.js
+++ src/features/home/pages/styles.js
@@ -6,8 +6,7 @@ export const Container = styled.div`
     align-items: center;
     justify-content: center;
     background: linear-gradient(170deg, ${variables.darkColor}, ${variables.mediumColor}, ${variables.lightColor}, ${variables.mediumColor}, ${variables.darkColor});
-    padding: 15px;
+    padding: 70px 15px 15px 15px;
     max-width: 1440px;
     height: 100vh;
-    margin-top: 55px;
 `
