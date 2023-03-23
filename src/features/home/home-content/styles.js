aaad814471f76a788ab74b3a46df27f10faa7f21
Jhony Freitas <jhony00._@hotmail.com>
2023-03-23 00:35:51 -0300

    Media query do home

--- src/features/home/home-content/styles.js
+++ src/features/home/home-content/styles.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 import { variables } from "../../../constants/variables"
+import { device } from '../../../constants/device';
 
 export const DivContainer = styled.div`
     display: flex;
+    justify-content: center;
     height: 98%;
     width: 100%;
 `
@@ -11,6 +13,10 @@ export const ContainerInfo = styled.div`
     justify-content: center;
     align-items: center;
     width: 40%;
+
+    @media ${device.tabletS} {
+        width: 100%;
+    }
 `
 
 export const DivInfo = styled.div`
@@ -23,13 +29,40 @@ export const DivInfo = styled.div`
         align-items: center;
         font-family: ${variables.secondaryFont};
         font-size: 22px;
+
+        @media ${device.laptop} {
+            font-size: 17px;
+        }
+        @media ${device.tablet} {
+            font-size: 15px;
+        }
     }
 
-    img{ width: 35px; }
+    img{
+        width: 35px;
+    }
 
-    h1{ font-size: 70px; }
-
-    h3{ font-size: 29px; }
+    h1{ 
+        font-size: 65px;
+        
+        @media ${device.laptop} {
+            font-size: 50px;
+        }
+        @media ${device.tablet} {
+            font-size: 42px;
+        }
+    }
+    
+    h3{ 
+        font-size: 29px; 
+        
+        @media ${device.laptop} {
+            font-size: 24px;
+        }
+        @media ${device.tablet} {
+            font-size: 19px;
+        }
+    }
 `
 
 export const ContainerImg = styled.div`
@@ -38,4 +71,15 @@ export const ContainerImg = styled.div`
     align-items: center;
     overflow: hidden;
     width: 60%;
+
+    
+    @media ${device.laptop} {
+        width: 55%;
+    }
+    @media ${device.tablet} {
+        width: 50%;
+    }
+    @media ${device.tabletS} {
+        display: none;
+    }
 `
