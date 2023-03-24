31df925157e1378ba38d250d6c231af59a752eb8
Jhony Freitas <jhony00._@hotmail.com>
2023-03-24 20:51:50 -0300

    Criando objeto de imagens

---
+++ src/features/projects/objects/url-image.js
@@ -0,0 +1,14 @@
+import fyloMobile from "../../../_assets/gifs/projeto-fylo-mobile.gif"
+import fylo from "../../../_assets/gifs/projeto-fylo.gif";
+import codeCafe from "../../../_assets/gifs/projeto-code-cafe.gif";
+
+export const Images = {
+    fylo : {
+        small: fyloMobile,
+        normal : fylo  
+    },
+    codeCafe : {
+        small: 'mobile',
+        normal : codeCafe  
+    }
+}
