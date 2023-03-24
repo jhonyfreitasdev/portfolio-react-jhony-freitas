6226e40f8b4a5ed568977a468499f8c84a8ad315
Jhony Freitas <jhony00._@hotmail.com>
2023-03-24 20:52:06 -0300

    Atualizando url dos projetos

--- src/features/projects/objects/projects-list.js
+++ src/features/projects/objects/projects-list.js
@@ -1,14 +1,11 @@
-import fyloMobile from "../../../_assets/gifs/projeto-fylo-mobile.gif"
-import fylo from "../../../_assets/gifs/projeto-fylo.gif";
-import codeCafe from "../../../_assets/gifs/projeto-code-cafe.gif";
-
+import { Images } from "./url-image"
 
 export const projectsList = [
     {
         id: '1',
         title: 'Fylo',
-        smallImage: fyloMobile,
-        image: fylo,
+        imageS: Images.fylo.small,
+        image: Images.fylo.normal,
         desc: 'Desafio feito do Fontend Mentor de uma landing page projetada com Flex e Grid, e a utilização de javascript para validação de email.',
         repoLink: 'https://github.com/jhonyfreitasdev/projeto-fylo',
         pageLink: 'https://jhonyfreitasdev.github.io/projeto-fylo/'
@@ -16,7 +13,8 @@ export const projectsList = [
     {
         id: '2',
         title: 'Code Café',
-        image: codeCafe,
+        imageS: 'mobile',
+        image: Images.codeCafe.normal,
         desc: 'Uma landing page de uma cafeteria usando HTML e CSS',
         repoLink: 'https://github.com/jhonyfreitasdev/projeto-code-cafe',
         pageLink: 'https://jhonyfreitasdev.github.io/projeto-code-cafe/'
