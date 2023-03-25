b2878dca182143c40d082dedb520a5cfe368c545
Jhony Freitas <jhony00._@hotmail.com>
2023-03-24 23:58:03 -0300

    Criando arquivo de estilização componente skills

---
+++ src/features/projects/skills/styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const List = styled.ul`
+    display: flex;
+    margin-bottom: 20px;
+`
+
+export const ListItem = styled.li`
+    margin: 12px;
+
+    i{
+        font-size: 30px;
+    }
+`
+export const Javascript = styled.i`
+    color: #ffff00;
+`
+export const React = styled.i`
+    color: #61DAFB;
+`
+export const Html = styled.i`
+    color: #ec4305;
+`
+export const Css = styled.i`
+    color: #0084ff;
+`
