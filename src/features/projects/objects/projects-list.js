522b0a3474dd718434107e18b6d8428bc293cecd
Jhony Freitas <jhony00._@hotmail.com>
2023-03-24 23:28:19 -0300

    Complementando projetos que estão no objeto

--- src/features/projects/objects/projects-list.js
+++ src/features/projects/objects/projects-list.js
@@ -1,22 +1,34 @@
-import { Images } from "./url-image"
+import { images } from "./url-image"
 
 export const projectsList = [
     {
         id: '1',
         title: 'Fylo',
-        imageS: Images.fylo.small,
-        image: Images.fylo.normal,
-        desc: 'Desafio feito do Fontend Mentor de uma landing page projetada com Flex e Grid, e a utilização de javascript para validação de email.',
+        imageS: images.fylo.small,
+        image: images.fylo.normal,
+        desc: 'Desafio feito do Fontend Mentor de uma landing page criada com Flex e Grid, e utilizando javascript para validação de email.',
         repoLink: 'https://github.com/jhonyfreitasdev/projeto-fylo',
-        pageLink: 'https://jhonyfreitasdev.github.io/projeto-fylo/'
+        pageLink: 'https://jhonyfreitasdev.github.io/projeto-fylo/',
+        skills: ['HTML', 'CSS', 'JavaScript']
     },
     {
         id: '2',
         title: 'Code Café',
-        imageS: 'mobile',
-        image: Images.codeCafe.normal,
-        desc: 'Uma landing page de uma cafeteria usando HTML e CSS',
+        imageS: images.codeCafe.small,
+        image: images.codeCafe.normal,
+        desc: 'Uma landing page de uma cafeteria.',
         repoLink: 'https://github.com/jhonyfreitasdev/projeto-code-cafe',
-        pageLink: 'https://jhonyfreitasdev.github.io/projeto-code-cafe/'
-    }
+        pageLink: 'https://jhonyfreitasdev.github.io/projeto-code-cafe/',
+        skills: ['HTML', 'CSS']
+    },
+    {
+        id: '3',
+        title: 'Pet Hope',
+        imageS: images.petHope.small,
+        image: images.codeCafe.normal,
+        desc: 'Uma Landing page de uma ONG para adoção de cachorros.',
+        repoLink: 'https://github.com/jhonyfreitasdev/projeto-code-cafe',
+        pageLink: 'https://jhonyfreitasdev.github.io/projeto-code-cafe/',
+        skills: ['HTML', 'CSS']
+    },
 ]
