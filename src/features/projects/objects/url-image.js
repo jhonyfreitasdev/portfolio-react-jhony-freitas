e9e2e007e844d1fd63f1935eb51029f35f4cfa84
Jhony Freitas <jhony00._@hotmail.com>
2023-03-24 23:22:55 -0300

    Adicionando imagens no objeto

--- src/features/projects/objects/url-image.js
+++ src/features/projects/objects/url-image.js
@@ -1,14 +1,58 @@
 import fyloMobile from "../../../_assets/gifs/projeto-fylo-mobile.gif"
 import fylo from "../../../_assets/gifs/projeto-fylo.gif";
+import codeCafeMobile from "../../../_assets/gifs/projeto-code-cafe-mobile.gif";
 import codeCafe from "../../../_assets/gifs/projeto-code-cafe.gif";
-
-export const Images = {
-    fylo : {
+import petHopeMobile from "../../../_assets/gifs/projeto-pet-hope-mobile.gif";
+import petHope from "../../../_assets/gifs/projeto-pet-hope.gif";
+import validacaoMobile from "../../../_assets/gifs/validacao-de-formulario-mobile.gif";
+import validacao from "../../../_assets/gifs/validacao-de-formulario.gif";
+import interactiveMobile from "../../../_assets/gifs/projeto-interactive-card-mobile.gif";
+import interactive from "../../../_assets/gifs/projeto-interactive-card.gif";
+import cordelMobile from "../../../_assets/gifs/projeto-cordel-mobile.gif";
+import cordel from "../../../_assets/gifs/projeto-cordel.gif";
+import carouselMobile from "../../../_assets/gifs/projeto-carousel-mobile.gif";
+import carousel from "../../../_assets/gifs/projeto-carousel.gif";
+import agenciaXYMobile from "../../../_assets/gifs/projeto-agencia-xyz-mobile.gif";
+import agenciaXY from "../../../_assets/gifs/projeto-agencia-xyz.gif";
+import agenciaXPMobile from "../../../_assets/gifs/projeto-agencia-xpto-mobile.gif";
+import agenciaXP from "../../../_assets/gifs/projeto-agencia-xpto.gif";
+ 
+export const images = {
+    fylo: {
         small: fyloMobile,
         normal : fylo  
     },
-    codeCafe : {
-        small: 'mobile',
+    codeCafe: {
+        small: codeCafeMobile,
         normal : codeCafe  
-    }
+    },
+    petHope: {
+        small: petHopeMobile,
+        normal: petHope
+    },
+    validacao: {
+        small: validacaoMobile,
+        normal: validacao
+    },
+    interactiveCard: {
+        small: interactiveMobile,
+        normal: interactive
+    },
+    cordel: {
+        small: cordelMobile,
+        normal: cordel
+    },
+    carousel: {
+        small: carouselMobile,
+        normal: carousel
+    },
+    agenciaXYZ: {
+        small: agenciaXYMobile,
+        normal: agenciaXY
+    },
+    agenciaXPTO: {
+        small: agenciaXPMobile,
+        normal: agenciaXP
+    },
+
 }
