a02ba4735cacdd634cde53be699e92a90c23af33
Jhony Freitas <jhony00._@hotmail.com>
2023-03-29 20:16:12 -0300

    Arrumando responsivo de ícones do projeto

--- src/features/projects/project/styles.js
+++ src/features/projects/project/styles.js
@@ -7,9 +7,15 @@ export const Info = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    text-align: center;
+    max-width: 97%;
     top: 50%;
     left: 50%;
     transform: translateX(-50%) translateY(-50%); 
+
+    @media ${device.laptopL} {
+        width: 500px;
+    }
 `
 
 export const GifI = styled.img`
@@ -24,11 +30,28 @@ export const GifS = styled.source`
 
 export const Title = styled.h2`
     font-size: 37px;
+
+    @media ${device.laptopL} {
+        font-size: 25px;
+    }
+    @media ${device.mobileM} {
+        font-size: 20px;
+    }
 `
 
 export const Description = styled.p`
+    text-align: center;
     font-size: 22px;
     margin: 40px 0 20px 0;
+
+    @media ${device.laptopL} {
+        margin: 20px 0 10px 0;
+        font-size: 18px;
+    }
+    @media ${device.mobileL} {
+        font-size: 15px;
+        margin: 6px 0 0 0;
+    }
 `
 
 export const Button = styled.a`
@@ -38,9 +61,21 @@ export const Button = styled.a`
     font-size: 18px;
     text-align: center;
     width: 120px;
-    padding: 7px 12px;
+    padding: 6px 0;
     margin: 0 15px;
     border-radius: 7px;
+
+    @media ${device.laptopL} {
+        width: 95px;
+        font-size: 16px;
+    }
+    @media ${device.mobileM} {
+        margin: 0 7px;
+    }
+    @media ${device.mobileS} {
+        width: 70px;
+        font-size: 14px;
+    }
 `
 
 export const GifContainer = styled.div`
@@ -74,14 +109,12 @@ export const GifContainer = styled.div`
         width: 270px;
         height: 317px;
     }
-    @media ${device.mobileS} {
-        width: 210px;
-        height: 246px;
-    }
-
+    
     &:hover {
-        width: 1200px;
-        height: 540px;
+        @media (min-width: 1216px) {    
+            width: 1200px;
+            height: 540px;
+        }
         ${GifI}{
             opacity: 0.2;
         }
