56f166985157ce43d6f8bd95181aa806ab85b0d7
Jhony Freitas <jhony00._@hotmail.com>
2023-03-29 20:15:20 -0300

    Adicionando projetos na lista

--- src/features/projects/objects/projects-list.js
+++ src/features/projects/objects/projects-list.js
@@ -3,30 +3,120 @@ import { images } from "./url-image"
 export const projectsList = [
     {
         id: '1',
+        title: 'API do Github',
+        // imageS: images..small,
+        // image: images..normal,
+        desc: 'Nesse site eu faço um request da Api do GitHub, e renderizo informações do perfil solicitado, como repositórios, seguidores e eventos.',
+        repoLink: 'https://github.com/jhonyfreitasdev/projeto-fetch-com-api-do-github',
+        pageLink: 'https://jhonyfreitasdev.github.io/projeto-fetch-com-api-do-github/',
+        skills: ['HTML', 'CSS', 'JavaScript']
+    },
+    /*{
+        id: '2',
+        title: 'Blog',
+        // imageS: images..small,
+        // image: images..normal,
+        desc: 'Um blog básico para trabalhar com o conceito de rotas no React.',
+        repoLink: 'https://github.com/jhonyfreitasdev/projeto-blog',
+        pageLink: '',
+        skills: ['React', 'JavaScript', 'Styled Components']
+    },*/
+    {
+        id: '3',
+        title: 'Pokédex',
+        // imageS: images..small,
+        // image: images..normal,
+        desc: 'Uma Pokédex na versão de slider, que mostra uma lista de Pokémons.',
+        repoLink: 'https://github.com/jhonyfreitasdev/projeto-pokemon-slider',
+        pageLink: 'https://jhonyfreitasdev.github.io/projeto-pokemon-slider/',
+        skills: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        id: '4',
+        title: 'Project Interactive Card',
+        imageS: images.interactiveCard.small,
+        image: images.interactiveCard.normal,
+        desc: 'Um formulário que vai atualizando as informações preenchidas nele, em um cartão modelo ao lado automaticamente. E possui validação de campos numéricos e vazios.',
+        repoLink: 'https://github.com/jhonyfreitasdev/project-interactive-card',
+        pageLink: 'https://jhonyfreitasdev.github.io/project-interactive-card/',
+        skills: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        id: '5',
+        title: 'Gerador de conselhos',
+        // imageS: images..small,
+        // image: images..normal,
+        desc: 'Um gerador de conselhos aleatórios, sendo fornecidos através de uma API.',
+        repoLink: 'https://github.com/jhonyfreitasdev/projeto-gerador-de-conselhos',
+        pageLink: 'https://jhonyfreitasdev.github.io/projeto-gerador-de-conselhos/',
+        skills: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        id: '6',
         title: 'Fylo',
         imageS: images.fylo.small,
         image: images.fylo.normal,
-        desc: 'Desafio feito do Fontend Mentor de uma landing page criada com Flex e Grid, e utilizando javascript para validação de email.',
+        desc: 'Desafio feito do Fontend Mentor de uma landing page criada com Flex e Grid, e utilizando javaScript para validação de email.',
         repoLink: 'https://github.com/jhonyfreitasdev/projeto-fylo',
         pageLink: 'https://jhonyfreitasdev.github.io/projeto-fylo/',
         skills: ['HTML', 'CSS', 'JavaScript']
     },
+    /*{
+        id: '7',
+        title: 'Contador de Pontos',
+        imageS: images..small,
+        image: images..normal,
+        desc: 'Em construção.',
+        repoLink: 'https://github.com/jhonyfreitasdev/projeto-contador-truco',
+        pageLink: 'https://jhonyfreitasdev.github.io/projeto-fylo/',
+        skills: ['React', 'JavaScript', 'Styled Components']
+    },*/
     {
-        id: '2',
+        id: '8',
+        title: 'Pet Hope',
+        imageS: images.petHope.small,
+        image: images.petHope.normal,
+        desc: 'Uma Landing page de uma ONG para adoção de cachorros.',
+        repoLink: 'https://github.com/jhonyfreitasdev/projeto-pet-hope',
+        pageLink: 'https://jhonyfreitasdev.github.io/projeto-pet-hope/',
+        skills: ['HTML', 'CSS']
+    },
+    {
+        id: '9',
+        title: 'Agência XPTO',
+        imageS: images.agenciaXPTO.small,
+        image: images.agenciaXPTO.normal,
+        desc: 'Uma landing page de divulgação de uma agência, com carousel de imagens contendo seus projetos.',
+        repoLink: 'https://github.com/jhonyfreitasdev/projeto-agencia-xpto',
+        pageLink: 'https://jhonyfreitasdev.github.io/projeto-agencia-xpto/',
+        skills: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        id: '10',
+        title: 'Agência XYZ',
+        imageS: images.agenciaXYZ.small,
+        image: images.agenciaXYZ.normal,
+        desc: 'Uma landing page criada para testar vários formatos de layouts, utilizando display Flex e Grid.',
+        repoLink: 'https://github.com/jhonyfreitasdev/projeto-agencia-xyz',
+        pageLink: 'https://jhonyfreitasdev.github.io/projeto-agencia-xyz/',
+        skills: ['HTML', 'CSS']
+    },
+    {
+        id: '11',
+        title: 'Cordel Moderno',
+        imageS: images.cordel.small,
+        image: images.cordel.normal,
+        desc: 'Uma poesia de Milton Duarte, construída em uma página, com efeito parallax.',
+        repoLink: 'https://github.com/jhonyfreitasdev/projeto-cordel',
+        pageLink: 'https://jhonyfreitasdev.github.io/projeto-cordel/',
+        skills: ['HTML', 'CSS']
+    },
+    {
+        id: '12',
         title: 'Code Café',
         imageS: images.codeCafe.small,
         image: images.codeCafe.normal,
         desc: 'Uma landing page de uma cafeteria.',
-        repoLink: 'https://github.com/jhonyfreitasdev/projeto-code-cafe',
-        pageLink: 'https://jhonyfreitasdev.github.io/projeto-code-cafe/',
-        skills: ['HTML', 'CSS']
-    },
-    {
-        id: '3',
-        title: 'Pet Hope',
-        imageS: images.petHope.small,
-        image: images.codeCafe.normal,
-        desc: 'Uma Landing page de uma ONG para adoção de cachorros.',
         repoLink: 'https://github.com/jhonyfreitasdev/projeto-code-cafe',
         pageLink: 'https://jhonyfreitasdev.github.io/projeto-code-cafe/',
         skills: ['HTML', 'CSS']
