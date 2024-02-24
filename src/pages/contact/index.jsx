e8f22c881119225a2191aec6c42d5d70cabda4b3
Jhony Freitas <jhony00._@hotmail.com>
2024-02-24 16:09:54 -0300

    Refatoração de contact

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -3,8 +3,9 @@ import { Form } from "../../components/form";
 import "./index.sass";
 
 export const Contact = () => {
-    return(
-        <div className="contact-container"> 
+    return (
+        <div className="contact-container">
+            <div className='blur-overlay'></div>
             <h2 className="title">Gostou do que viu? Entre em contato!</h2>
             <SocialMedia />
             <Form />
